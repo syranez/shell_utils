8f7e848350bb755940b7f00e67b2e52c11b2acbd
Michael Nowack <syranez@minaga-church.de>
2012-04-01 14:52:37 +0200

    Downloads the latest picture from redditpic.

---
+++ latestRedditpic.sh
@@ -0,0 +1,47 @@
+#! /usr/bin/env sh
+#
+# downloads the latest picture of a reddpics subreddit
+#
+# Usage:
+#   ./latestRedditpic.sh
+
+# Subreddit
+SUB="funny"
+
+# Base URI of reddpics
+BASE="http://reddpics.com"
+
+# Uri of the site of which you want to download pictures
+URI="${BASE}/r/${SUB}/"
+
+# Path to write the latest file
+OUTPUT="/tmp/latest.jpg";
+
+# retrieves the local uri of the latest picture
+#
+# @outputs local uri of latest picture
+getUriLatestPicture () {
+
+    local latest=$(wget -q "${URI}" -O - | tr ">" "\n" | grep '<img src="/' | sed 's/.*src="//g' | sed 's/".*//g' | head -n 1);
+
+    echo "${BASE}${latest}";
+}
+
+# gets an image from an uri
+#
+# @param string uri
+# @writes /tmp/latest.jpg
+get () {
+
+    local uri="${1}";
+
+    wget -q "${uri}" -O ${OUTPUT};
+
+    return $?;
+}
+
+latest=$(getUriLatestPicture);
+
+get "${latest}";
+
+return $?;
