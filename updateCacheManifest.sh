9385e0bf2784458b30bab246f2e7c4f0d2d577ae
Michael Nowack <syranez@minaga-church.de>
2012-04-01 19:01:28 +0200

    Updates the version of an application cache manifest.

---
+++ updateCacheManifest.sh
@@ -0,0 +1,27 @@
+#! /usr/bin/env sh
+#
+# updateѕ the version string of an application cache manifest.
+#
+# The version string must have this syntax:
+#
+#    > # v<number>
+#
+# Usage:
+#   ./updateCacheManifest.sh
+
+# manifest file
+MANIFEST="foobar.manifest";
+
+update () {
+
+    local count=$(grep "# v" "${MANIFEST}" | sed 's/# v//g');
+    local newcount=$((count + 1))
+
+    sed -i -e"s/# v${count}/# v${newcount}/" "${MANIFEST}";
+
+    return $?
+}
+
+update;
+
+exit $?;
